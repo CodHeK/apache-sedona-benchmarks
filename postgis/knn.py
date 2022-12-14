a655acbf5d3a2187ba89f6f6b1af7eb974a3499f
CodheK <gaganganapathyas@gmail.com>
2022-12-14 18:09:29 -0500

    Add postgis methods for polygons

--- postgis/knn.py
+++ postgis/knn.py
@@ -3,22 +3,28 @@ import psycopg2
 from postgis.psycopg import register
 import time, pprint
 import pandas as pd
+from shapely import wkt
+import numpy as np
 
 db = psycopg2.connect(dbname="benchmark", user="postgres", password="sour punk")
 register(db)
 cursor = db.cursor()
 
 k_vals = [ 1, 5, 10, 20, 30, 50, 100, 500, 1000 ]
+
+'''
+    Points:
+'''
 coords = [73.512247, 4.083805]
 
-def knn(t, k):
+def knn_points(t, k):
     sql = "SELECT ST_Distance(point, 'SRID=4326;POINT({0} {1})'::geometry) as d FROM {2} ORDER BY point <-> 'SRID=4326;POINT({0} {1})'::geometry LIMIT {3}".format(coords[0], coords[1], t, k)
     pd.read_sql(sql, db)
 
 t = 'all_points_1K'
 len = 1000
 
-output_file = open('knn.csv', 'w+')
+output_file = open('knn_points.csv', 'w+')
 pp = pprint.PrettyPrinter(indent=4, compact=True, stream=output_file)
 
 
@@ -28,7 +34,7 @@ utils.drop_index(db, cursor, t, 'point')
 s = time.time()
 for k in k_vals:
     s = time.time()
-    knn(t, k)
+    knn_points(t, k)
     d = time.time() - s
     pp.pprint(str(k) + "," + str(round(d, 3)))
 
@@ -39,6 +45,45 @@ utils.create_index(db, cursor, t, 'point')
 s = time.time()
 for k in k_vals:
     s = time.time()
-    knn(t, k)
+    knn_points(t, k)
+    d = time.time() - s
+    pp.pprint(str(k) + "," + str(round(d, 3)))
+
+
+
+'''
+    Polygons:
+'''
+polygon = open("../sedona_osm_data/knn_polygon.tsv", "r").read()
+
+def knn_polygon(t, k):
+    sql = "SELECT ST_Distance(polygon, (ST_SetSRID(ST_GeomFromText('{2}'), 4326))) as d FROM {0} ORDER BY polygon <-> (ST_SetSRID(ST_GeomFromText('{2}'), 4326)) LIMIT {1}".format(t, k, polygon)
+    pd.read_sql(sql, db)
+
+t = 'all_source_1K'
+len = 1000
+
+output_file = open('knn_polygons.csv', 'w+')
+pp = pprint.PrettyPrinter(indent=4, compact=True, stream=output_file)
+
+
+pp.pprint("No index")
+utils.drop_index(db, cursor, t, 'polygon')
+
+s = time.time()
+for k in k_vals:
+    s = time.time()
+    knn_polygon(t, k)
+    d = time.time() - s
+    pp.pprint(str(k) + "," + str(round(d, 3)))
+
+
+pp.pprint("R-Tree index")
+utils.create_index(db, cursor, t, 'polygon')
+
+s = time.time()
+for k in k_vals:
+    s = time.time()
+    knn_polygon(t, k)
     d = time.time() - s
     pp.pprint(str(k) + "," + str(round(d, 3)))
--- postgis/range.py
+++ postgis/range.py
@@ -10,7 +10,11 @@ cursor = db.cursor()
 
 envelope = (-85.01, -60.01, 34.01, 50.01)
 
-def range_query(t):
+
+'''
+    Points:
+'''
+def range_query_point(t):
     sql = "SELECT COUNT(*) FROM {0} WHERE ST_Intersects(point, (ST_SetSRID(ST_Envelope('LINESTRING({1} {2}, {3} {4})'::geometry), 4326)))".format(t, *envelope)
     pd.read_sql(sql, db)
 
@@ -18,7 +22,7 @@ def range_query(t):
 t = 'all_points_1K'
 len = 1000
 
-output_file = open('range.csv', 'w+')
+output_file = open('range_points.csv', 'w+')
 pp = pprint.PrettyPrinter(indent=4, compact=True, stream=output_file)
 
 
@@ -26,7 +30,7 @@ pp.pprint("No index")
 utils.drop_index(db, cursor, t, 'point')
 
 s = time.time()
-range_query(t)
+range_query_point(t)
 d = time.time() - s
 pp.pprint(str(len) + "," + str(round(d, 3)))
 
@@ -35,6 +39,39 @@ pp.pprint("R-Tree index")
 utils.create_index(db, cursor, t, 'point')
 
 s = time.time()
-range_query(t)
+range_query_point(t)
+d = time.time() - s
+pp.pprint(str(len) + "," + str(round(d, 3)))
+
+
+
+'''
+    Polygons:
+'''
+def range_query_polygon(t):
+    sql = "SELECT COUNT(*) FROM {0} WHERE ST_Intersects(polygon, (ST_SetSRID(ST_Envelope('LINESTRING({1} {2}, {3} {4})'::geometry), 4326)))".format(t, *envelope)
+    pd.read_sql(sql, db)
+
+
+t = 'all_source_1K'
+len = 1000
+
+output_file = open('range_polygon.csv', 'w+')
+pp = pprint.PrettyPrinter(indent=4, compact=True, stream=output_file)
+
+pp.pprint("No index")
+utils.drop_index(db, cursor, t, 'polygon')
+
+s = time.time()
+range_query_polygon(t)
+d = time.time() - s
+pp.pprint(str(len) + "," + str(round(d, 3)))
+
+
+pp.pprint("R-Tree index")
+utils.create_index(db, cursor, t, 'polygon')
+
+s = time.time()
+range_query_polygon(t)
 d = time.time() - s
 pp.pprint(str(len) + "," + str(round(d, 3)))
